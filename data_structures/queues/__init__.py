db5f2cb3e7dbcb59e78cbba7cf5a21d01c2edb0d
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-03-16 04:33:30 +0500

    Added tests for double linked list

--- data_structures/queues/__init__.py
+++ data_structures/queues/__init__.py
@@ -1,9 +1,11 @@
 from .simple import SimpleQueue
 from .static_circular import StaticCircularQueue
 from .dynamic_circular import DynamicCircularQueue
+from .dequeue import Deque
 
 __all__ = [
     'SimpleQueue',
     'StaticCircularQueue',
     'DynamicCircularQueue',
+    'Deque',
 ]
