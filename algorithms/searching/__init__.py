c10203ceee413af55e79ce12611961415c2675ac
Umidjon Khodjaev <iexistedonlyforyou@gmail.com>
2026-04-13 19:36:47 +0500

    completed searching algorithms

---
+++ algorithms/searching/__init__.py
@@ -0,0 +1,19 @@
+from .binary_search import (
+    iterative_binary_search,
+    recursive_binary_search,
+)
+from .sliding_window import max_sum_subarray
+from .two_pointers import (
+    is_palindrome,
+    pair_sum_search,
+    remove_duplicates,
+)
+
+__all__ = [
+    "iterative_binary_search",
+    "recursive_binary_search",
+    "is_palindrome",
+    "pair_sum_search",
+    "remove_duplicates",
+    "max_sum_subarray",
+]
